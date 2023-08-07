40a037f2004c1a9a43e1f3cdaa17c4dc49c69cdb
Karen Cinca <karencinca1@gmail.com>
2023-08-06 22:05:38 -0300

    update name and email

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,14 +1,59 @@
+const { hash } = require('bcryptjs')
 const AppError = require('../utils/AppError')
 
-class UserController {
-    create(request, response) {
-        const { name, email, password } = request.body
+const sqliteConnection = require('../database/sqlite')
 
-        if (!name) {
-            throw new AppError('Informar nome')
+class UserController {
+    async create(request, response) {
+        const { name, email, password, isAdmin } = request.body
+
+        const database = await sqliteConnection()
+        const checkUserExists = await database.get('SELECT * FROM users WHERE email = (?)', [email])
+
+        if (checkUserExists) {
+            throw new AppError('Email já cadastrado')
         }
 
-        response.status(201).json({ name, email, password })
+        const hashedPassword = await hash(password, 8)
+
+        await database.run(
+            'INSERT INTO users (name, email, password, is_admin) VALUES (?, ?, ?, ?)', 
+            [name, email, hashedPassword, isAdmin]
+            )
+
+        return response.status(201).json()
+    }
+
+    async update(request, response) {
+        const { name, email } = request.body
+        const { id } = request.params
+
+        const database = await sqliteConnection()
+        const user = await database.get('SELECT * FROM users WHERE id = (?)', [id])
+
+        if (!user) {
+            throw new AppError('Usuário não encontrado')
+        }
+
+        const userWithUpdatedEmail = await database.get('SELECT * FROM users WHERE email = (?)', [email])
+
+        if (userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id) {
+            throw new AppError('E-mail já cadastrado.')
+        }
+
+        user.name = name
+        user.email = email
+
+        await database.run(`
+            UPDATE users SET
+            name = ?,
+            email = ?,
+            updated_at = ?
+            WHERE id = ?`,
+            [user.name, user.email, new Date(), id]
+            )
+
+            return response.status(200).json()
     }
 }
 
